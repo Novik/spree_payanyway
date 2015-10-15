87d2f30b609cfb9989d2a5e2d6372051fed1fd73
Novik <novik65@gmail.com>
2015-10-15 15:38:18 +0300

    minor fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Spree::Core::Engine.routes.draw do
   # Add your extension routes here
 
   namespace :gateway do
-    get '/payanyway/result'                => 'payanyway#result',  as: :payanyway_result
+    post '/payanyway/result'               => 'payanyway#result',  as: :payanyway_result
     get '/payanyway/success/:order_id'     => 'payanyway#success', as: :payanyway_success
     get '/payanyway/fail/:order_id'        => 'payanyway#fail',    as: :payanyway_fail
   end
